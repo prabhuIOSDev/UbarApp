24c921eafb767cd21950c5e180927a33ba24d30a
prabhuIOSDev <prabhuiosdeveloper@gmail.com>
2025-07-22 17:06:40 +0530

    added

--- UbarApp/Core/View/UbarMapViewReprentable.swift
+++ UbarApp/Core/View/UbarMapViewReprentable.swift
@@ -5,4 +5,53 @@
 //  Created by Prabhu Devar on 22/07/25.
 //
 
-import Foundation
+import SwiftUI
+import MapKit
+// zxgc
+
+
+struct UbarMapViewReprentable: UIViewRepresentable {
+   
+    
+    
+    
+    
+    let mapView = MKMapView()
+    
+    func makeUIView(context: Context) -> MKMapView {
+        mapView.mapType = .standard
+        mapView.showsUserLocation = true
+        mapView.isRotateEnabled = false
+        mapView.userTrackingMode = .follow
+        
+        
+        return mapView
+    }
+    
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> MapCoordinater{
+        return MapCoordinater(present: self)
+        
+    }
+    
+}
+
+extension UbarMapViewReprentable{
+
+    class MapCoordinater: NSObject, MKMapViewDelegate{
+        
+        let present : UbarMapViewReprentable
+        
+         init(present : UbarMapViewReprentable){
+            self.present = present
+            super.init()
+        }
+        
+    }
+    
+    
+}
